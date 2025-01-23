19d041ca86c49b64d129a84143d37f0070ed2ddb
UmairM13 <22493168@stu.mmu.ac.uk>
2025-01-23 23:42:30 +0000

    added recommendations

--- frontend/vue-project/src/router/index.js
+++ frontend/vue-project/src/router/index.js
@@ -1,32 +1,44 @@
-import {createRouter, createWebHistory} from 'vue-router';
-import Home from '../views/Home.vue';
-import Search from '../views/Search.vue';
-import AdvanceFilter from '../views/AdvanceFilter.vue';
-import MoreInfo from '../views/MoreInfo.vue';
-import Recommendation from '../views/Recommendation.vue';
-import Login from '../views/Login.vue';
-import Signup from '@/views/Signup.vue';
-import BMICalculator from '@/views/BMICalculator.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Search from "../views/Search.vue";
+import AdvanceFilter from "../views/AdvanceFilter.vue";
+import MoreInfo from "../views/MoreInfo.vue";
+import Recommendation from "../views/Recommendation.vue";
+import Login from "../views/Login.vue";
+import Signup from "@/views/Signup.vue";
+import BMICalculator from "@/views/BMICalculator.vue";
+
+const ifAuthenticated = (to, from, next) => {
+  const loggedIn = localStorage.getItem("session_token");
+  if (loggedIn) {
+    next();
+    return;
+  }
+  next("login");
+};
 
 // Define your routes
 const routes = [
-  { path: '/', component: Home }, 
-  { path: '/search', component: Search }, 
-  { path: '/filter', component: AdvanceFilter }, 
-  { path: '/foods/:foodId', component: MoreInfo }, 
-  { path: '/recommendation', component: Recommendation }, 
-  { path: '/login', component: Login}, 
-  { path: '/signup', component: Signup}, 
-  { path: '/bmiCalculator', component: BMICalculator}, 
+  { path: "/", component: Home },
+  { path: "/search", component: Search },
+  { path: "/filter", component: AdvanceFilter },
+  { path: "/foods/:foodId", component: MoreInfo },
+  {
+    path: "/recommendation",
+    component: Recommendation,
+    beforeEnter: ifAuthenticated,
+  },
+  { path: "/login", component: Login },
+  { path: "/signup", component: Signup },
+  { path: "/bmiCalculator", component: BMICalculator },
 
   // Add other routes as needed
 ];
 
 // Create the router instance
-const router= createRouter({
-        history:createWebHistory(),
-        routes,
-    })
-  
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
-    export default router;
+export default router;
