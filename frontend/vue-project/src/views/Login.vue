f5dcc50eb33f9fc0d1b49e3941da5c6f44b08a9f
UmairM13 <22493168@stu.mmu.ac.uk>
2025-01-23 23:23:27 +0000

    updated design

--- frontend/vue-project/src/views/Login.vue
+++ frontend/vue-project/src/views/Login.vue
@@ -3,7 +3,7 @@
     <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
       <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
         <h1 class="title-font font-medium text-3xl text-gray-900">Slow-carb next level shoindcgoitch ethical authentic, poko scenester</h1>
-        <p class="leading-relaxed mt-4">Poke slow-carb mixtape knausgaard, typewriter street art gentrify hammock starladder roathse. Craies vegan tousled etsy austin.</p>
+        <p class="leading-relaxed mt-4">Poke slow-carb food knausgaard, ignore this please it is for asthetic purposes. Craies vegan tousled more food.</p>
       </div>
       <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
         <form @submit.prevent="handleLogin">
