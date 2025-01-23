9e6bd2c26933dc59e2ed1a0c39a04e33e22f1ce5
UmairM13 <22493168@stu.mmu.ac.uk>
2025-01-23 23:42:06 +0000

    added getbmi endpoint

--- frontend/vue-project/src/services/userservices.js
+++ frontend/vue-project/src/services/userservices.js
@@ -1,14 +1,14 @@
 const login = (email, password) => {
-    return fetch("http://localhost:3333/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        "email": email,
-        "password": password,
-      }),
-    })
+  return fetch("http://localhost:3333/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
     .then((response) => {
       if (response.status === 200) {
         return response.json();
@@ -20,29 +20,28 @@ const login = (email, password) => {
       console.log(" this responese: " + resJson);
       return resJson;
     })
-  
+
     .catch((err) => {
       console.log(err);
       return Promise.reject(err);
     });
-  };
+};
 
-
-  const signup = (first_name, last_name, email, password, age, gender) => {
-    return fetch("http://localhost:3333/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        "first_name":first_name ,
-        "last_name": last_name,
-        "email": email,
-        "password": password,
-        "age": age,
-        "gender": gender,
-      }),
-    })
+const signup = (first_name, last_name, email, password, age, gender) => {
+  return fetch("http://localhost:3333/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      password: password,
+      age: age,
+      gender: gender,
+    }),
+  })
     .then((response) => {
       if (response.status === 201) {
         return response.json();
@@ -54,35 +53,59 @@ const login = (email, password) => {
       console.log(" this responese: " + resJson);
       return resJson;
     })
-  
+
     .catch((err) => {
       console.log(err);
       return Promise.reject(err);
     });
-  };
-  
-  // Logout of a user accoutn
-const logout = () => {
-    return fetch("http://localhost:3333/logout", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Authorization": localStorage.getItem("session_token"),
-      },
-    }).then((response) => {
-      // Clear the session token
-      localStorage.removeItem("session_token");
-      if (!response.ok) {
-        throw new Error("Logout failed");
-      }
-      // Return
-      return; 
-    });
-  };
+};
 
-  export const userServices = {
-    signup,
-    login,
-    logout
-  };
-  
+// Logout of a user accoutn
+const logout = () => {
+  return fetch("http://localhost:3333/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": localStorage.getItem("session_token"),
+    },
+  }).then((response) => {
+    // Clear the session token
+    localStorage.removeItem("session_token");
+    if (!response.ok) {
+      throw new Error("Logout failed");
+    }
+    // Return
+    return;
+  });
+};
+
+const getBmi = () => {
+  return fetch("http://localhost:3333/bmi", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": localStorage.getItem("session_token"),
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else if (response.status === 500) {
+        throw "Internal Server Error";
+      }
+    })
+    .then((resJson) => {
+      return resJson;
+    })
+    .catch((err) => {
+      console.log(err);
+      return Promise.reject(err);
+    });
+};
+
+export const userServices = {
+  signup,
+  login,
+  logout,
+  getBmi,
+};
